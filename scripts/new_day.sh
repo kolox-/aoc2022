36f84f603d0c4e0b5d08bc8898c15c8167a703b1
kolox- <colinbutler1995@gmail.com>
2022-11-30 23:23:10 +0000

    Add a dumb script to do the day setup for a new day

---
+++ scripts/new_day.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+set -e
+# create structure for a new day
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DAY_NUM=$1
+DAY="day0${DAY_NUM}"
+NEW_FOLDER="../${DAY}"
+
+pushd $SCRIPT_DIR
+mkdir ${NEW_FOLDER}
+touch ${NEW_FOLDER}/${DAY}.cpp
+cp ../day01/CMakeLists.txt ${NEW_FOLDER}
+echo "add_subdirectory(${DAY})" >> ../CMakeLists.txt
+./get.sh ${DAY_NUM} > ${NEW_FOLDER}/long.txt
+popd
