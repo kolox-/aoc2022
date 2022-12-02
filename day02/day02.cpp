00098f2e5737a251059daea6b9706ac215f70ca8
kolox- <colinbutler1995@gmail.com>
2022-12-02 14:56:04 +0000

    Day2 solution

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -1,15 +1,87 @@
 #include <utils.hpp>
 
+using Pair_T = std::pair<char, char>;
+
+constexpr char THEIR_BASE = 'A';
+constexpr char OUR_BASE = 'X';
+
+constexpr int WIN_PTS = 6;
+constexpr int DRAW_PTS = 3;
+constexpr int LOSS_PTS = 0;
+
+constexpr int NUM_CHOICES = 3;
+
+enum class Choice { ROCK, PAPER, SCISSORS };
+
+enum class Outcome { DRAW, WIN, LOSE };
+
+constexpr int rotate(int to_rotate, int steps) {
+    // Since the precedence is circular (rock < paper < scissors < rock), we
+    // often need to rotate the enum values. In this case there are always three
+    // values in both enums so we can hardcode.
+    return (to_rotate + steps) % NUM_CHOICES;
+};
+
+constexpr bool beats(Choice a, Choice b) {
+    // Each choice beats the one behind it (ie. rotate by 2) in our enum
+    return rotate(static_cast<int>(a), 2) == static_cast<int>(b);
+}
+
+constexpr int score(Choice theirs, Choice ours) {
+    // choice bonus starts from 1 and is ordered the same as our enum
+    constexpr int base_bonus = 1;
+    const int bonus = static_cast<int>(ours) + base_bonus;
+    if (beats(ours, theirs)) {
+        return WIN_PTS + bonus;
+    } else if (beats(theirs, ours)) {
+        return LOSS_PTS + bonus;
+    } else {
+        return DRAW_PTS + bonus;
+    }
+}
+
+constexpr Outcome outcome_from_char(char input) {
+    // The order given is X, Y, Z : LOSE, DRAW, WIN
+    // Need to rotate them by +2 to match our Outcomes enum for get_move()
+    auto zeroed_outcome = input - OUR_BASE;
+    return static_cast<Outcome>(rotate(zeroed_outcome, 2));
+}
+
+constexpr Choice get_move(Choice theirs, Outcome outcome) {
+    // Outcome enum is ordered such that the value is the Choice rotation needed
+    // for that outcome (eg. to WIN (enum val 1), we need to select the next
+    // (+1) choice in the Choices enum
+    auto result_int =
+        rotate(static_cast<int>(theirs), static_cast<int>(outcome));
+    return static_cast<Choice>(result_int);
+}
+
 int part_one(const std::string& filename) {
     std::fstream infile(filename);
+    std::vector<Pair_T> pairs;
+    cb::parse_pairs(infile, pairs);
 
-    return -1;
+    auto score_outcome = [](int acc, auto& pair) {
+        auto theirs = static_cast<Choice>(pair.first - THEIR_BASE);
+        auto ours = static_cast<Choice>(pair.second - OUR_BASE);
+        return acc + score(theirs, ours);
+    };
+    return std::accumulate(pairs.begin(), pairs.end(), 0, score_outcome);
 }
 
 int part_two(const std::string& filename) {
     std::fstream infile(filename);
+    std::vector<Pair_T> pairs;
+    cb::parse_pairs(infile, pairs);
 
-    return -1;
+    auto score_outcome = [](int acc, auto& pair) {
+        auto theirs = static_cast<Choice>(pair.first - THEIR_BASE);
+        auto outcome = outcome_from_char(pair.second);
+        auto ours = get_move(theirs, outcome);
+        return acc + score(theirs, ours);
+    };
+
+    return std::accumulate(pairs.begin(), pairs.end(), 0, score_outcome);
 }
 
 int main(int argc, char* argv[]) {
@@ -20,8 +92,7 @@ int main(int argc, char* argv[]) {
     std::string path{argv[1]};
     std::cout << "Filename: " << path << "\n\n";
     std::cout << "Part one: \n" << part_one(path) << "\n\n";
-    // short: , long: 
+    // short: 15, long: 8392
     std::cout << "Part two: \n" << part_two(path) << std::endl;
-    // short: , long: 
-
+    // short: 12, long: 10116
 }
--- utils/utils.hpp
+++ utils/utils.hpp
@@ -18,6 +18,16 @@ void parse_stream(std::istream& s, std::vector<T>& out) {
     }
 }
 
+template <typename T1, typename T2>
+void parse_pairs(std::istream& s, std::vector<std::pair<T1, T2> >& out) {
+    T1 a;
+    T2 b;
+    while (s >> a >> b) {
+        // std::cout << a << "," << b << std::endl;
+        out.emplace_back(a, b);
+    }
+}
+
 inline std::vector<int> parse_stream_ints_until_newline(std::istream& s) {
     std::vector<int> out;
     std::string raw;
